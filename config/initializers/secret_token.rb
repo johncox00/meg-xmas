afd870d47b3fc6d5f740bde3dac76cf266916b81
John Cox <john@mach19.com>
2012-12-22 10:44:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegXmas::Application.config.secret_token = 'fcef39da040ee0b6d9618e3ddab2e073d3dd91e40fbf1c3af835ac0cea040605bc24a6e29e652f9185e7858de6fa2a866ddc41cef984ce0af033aa8bfbc0f8ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MegXmas::Application.config.session_store :cookie_store, key: '_meg-xmas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MegXmas::Application.config.session_store :active_record_store
